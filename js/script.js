cc54feb2ddbdf9a5e99f953f7120e7ca24bb86c3
katmsc <ekat.piv@gmail.com>
2019-08-05 14:56:39 +0300

    add header

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var wrapper = document.querySelector(".introduction");
+var text = document.querySelector(".introduction-text");
+
+
+var textCont = text.textContent;
+text.style.display = "none";
+
+for (var i = 0; i < textCont.length; i++) {
+  (function(i) {
+    setTimeout(function() {
+      // Created textNode to append
+      var texts = document.createTextNode(textCont[i])
+      var span = document.createElement('span');
+      span.appendChild(texts);
+
+      span.classList.add("wave");
+      wrapper.appendChild(span);
+
+    }, 75 * i);
+  }(i));
+}
